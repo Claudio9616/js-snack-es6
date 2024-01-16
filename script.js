389a4ca68f7586a8a2a84b5d6d608b44ce5edfdb
Claudio9616 <clares9611@gmail.com>
2024-01-16 16:47:55 +0100

    snacks 1 version 1.4

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-;
+// SNACKS1
+// 1 creo una variabile per il nome del tavolo
+// 2 creo un'array con il nome degli invitati
+// 3 uso il metodo map per creare una lista di array di oggetti, aggiungendo
+// il nome del tavolo e il numero del posto
+// 4 stampo in console
 
+// 1
+const tableName = 'Tavolo Vip'
+// 2
 const guests = [
   'Brad Pitt',
   'Johnny Depp',
@@ -11,15 +19,14 @@ const guests = [
   'George Clooney',
   'Amal Clooney',
   'Maneskin'
-];
-const guestsList = guests.map((guest, index) => {
-    let guestsName = guest
-    let place = index
-    const tableName = 'Tavolo Vip' 
+]
+// 3
+const guestsList = guests.map((guest, index) => {     
     return {
         tableName: tableName,
-        guestsName: guestsName,
-        place: place
+        guestsName: guest,
+        place: ++index
     }
 })
+// 4
 console.log(guestsList)
