22d3a3cc8776936761aca6ffcade042e602d5a8d
Claudio9616 <clares9611@gmail.com>
2024-01-16 15:49:57 +0100

    snacks 1

---
+++ script.js
@@ -0,0 +1,18 @@
+const tableName = 'Tavolo Vip';
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
+const guestsList = guests.map(guest => {
+    return guest
+})
+console.log(guestsList)
