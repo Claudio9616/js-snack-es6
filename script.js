df112c366008bedbb2d2b31ce6f3cf9dc3dc66f5
Claudio9616 <clares9611@gmail.com>
2024-01-16 17:20:27 +0100

    completed snacks 2

--- script.js
+++ script.js
@@ -31,15 +31,15 @@ const guestsList = guests.map((guest, index) => {
 // 4
 console.log(guestsList)
 
-// Id|Name|Grades
-// 213|Marco della Rovere|78
-// 110|Paola Cortellessa|96
-// 250|Andrea Mantegna|48
-// 145|Gaia Borromini|74
-// 196|Luigi Grimaldello|68
-// 102|Piero della Francesca|50
-// 120|Francesca da Polenta|84
 
+// SNACKS2
+// 1 creo un'array di oggetti contenenti i valori di ID, NOME e VOTI
+// 2 creo una lista solo con gli studenti che hanno un voto superiore a 70
+// 3 creo una lista di sudenti che hanno un voto superiore a 70 e un ID superiore a 120
+// 4 creo una lista solo con i nomi degli studenti, srampati in maiuscolo
+// 5 stampo in console
+
+// 1
 const students = [
     {id:'213', name: 'Marco della Rovere', vote:'78'},
     {id:'110', name: 'Paola Cortellessa', vote:'96'},
@@ -49,6 +49,15 @@ const students = [
     {id:'102', name: 'Piero della Francesca', vote:'50'},
     {id:'120', name: 'Francesca da Polenta', vote:'84'}
 ]
+// 2
 const over70Votes = students.filter(({vote}) => vote >= 70)
-const over120Id = students.filter(({id}) => id >= 120)
-console.log(over70Votes, over120Id)
+// 3
+const over120Id = students.filter(student => {
+    if(student.id >= 120 && student.vote >= 70){
+        return true
+    }
+})
+// 4
+const names = students.map(({name}) => name.toUpperCase())
+// 5
+console.log(over70Votes, over120Id, names)
