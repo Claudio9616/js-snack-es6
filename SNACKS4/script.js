032ba91d5e698309dda7c1792a494ab07617f1c4
Claudio9616 <clares9611@gmail.com>
2024-01-17 17:16:52 +0100

    snacks 4 add forEach and start filter

--- SNACKS4/script.js
+++ SNACKS4/script.js
@@ -1,5 +1,20 @@
 // snacks 4
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Generare numeri random al posto degli 0 nelle proprietà punti fatti e falli subiti.
+// Infine, usando il destructuring, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console
 const premierLeague = [
-    {name: 'chelsea', points: 0, }    
+    {name: 'Chelsea', points: 0, fouls: 0},
+    {name: 'Arsenal', points: 0, fouls: 0},
+    {name: 'Manchester City', points: 0, fouls: 0},
+    {name: 'Manchester City', points: 0, fouls: 0},
+    {name: 'Liverpool', points: 0, fouls: 0}   
 ]
+let randomPoints;
+let randomFouls;
+premierLeague.forEach(clubs => {
+    randomPoints = Math.floor(Math.random() * 100) + 1
+    randomFouls = Math.floor(Math.random() * 100) + 1
+    console.log('randomPoints: ', randomPoints)
+    console.log('randomFouls: ', randomFouls)
+})
+const newPremierLeague = premierLeague.filter()
