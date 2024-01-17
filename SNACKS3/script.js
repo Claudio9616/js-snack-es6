177b03c7dfca4c411bf681b4c989f89a5a0378af
Claudio9616 <clares9611@gmail.com>
2024-01-17 18:42:53 +0100

    add new versions of SNACKS 3 and 4 to fix

--- SNACKS3/script.js
+++ SNACKS3/script.js
@@ -7,6 +7,7 @@ bicycles = [
     {name: 'bianchi', weight: 30},
     {name: 'rossi', weight: 5}
 ]
+
 const minWeight = bicycles.find(({weight}) => weight === 5)
 console.log(minWeight)
 
--- SNACKS4/script.js
+++ SNACKS4/script.js
@@ -6,15 +6,31 @@ const premierLeague = [
     {name: 'Chelsea', points: 0, fouls: 0},
     {name: 'Arsenal', points: 0, fouls: 0},
     {name: 'Manchester City', points: 0, fouls: 0},
-    {name: 'Manchester City', points: 0, fouls: 0},
+    {name: 'Manchester United', points: 0, fouls: 0},
     {name: 'Liverpool', points: 0, fouls: 0}   
 ]
 let randomPoints;
 let randomFouls;
-premierLeague.forEach(clubs => {
+const newPremierLeague = premierLeague.map(({name}) => {
+    return name
+})
+console.log(newPremierLeague, 'primo map')
+
+
+
+newPremierLeague.forEach(clubsRandom => {
     randomPoints = Math.floor(Math.random() * 100) + 1
     randomFouls = Math.floor(Math.random() * 100) + 1
     console.log('randomPoints: ', randomPoints)
     console.log('randomFouls: ', randomFouls)
+    newPremierLeague.push(randomFouls)
 })
-const newPremierLeague = premierLeague.map()
+console.log(newPremierLeague, 'secondo')
+
+
+const finalPremierLeague = newPremierLeague.map(({name}) => {
+    return {
+        name: name
+    }
+})
+console.log(finalPremierLeague)
