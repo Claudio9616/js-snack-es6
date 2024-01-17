2bd71c40ce6726492e4722cd61d6ed923ea0fdc3
Claudio9616 <clares9611@gmail.com>
2024-01-17 16:30:31 +0100

    snacks 3

--- script.js
+++ script.js
@@ -61,3 +61,14 @@ const over120Id = students.filter(student => {
 const names = students.map(({name}) => name.toUpperCase())
 // 5
 console.log(over70Votes, over120Id, names)
+
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e peso.
+ 
+bicycles = [
+    {name: 'cosentino', weight: 10},
+    {name: 'lombardo', weight: 20},
+    {name: 'bianchi', weight: 30},
+    {name: 'rossi', weight: 5}
+]
+const minWeight = bicycles.find(({weight}) => weight === 5)
+console.log(minWeight)
